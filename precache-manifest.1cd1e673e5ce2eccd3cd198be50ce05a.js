9e78d2125a69348974232c6f8857af974e551d39
bjgribb <bjgribb@gmail.com>
2019-07-08 10:47:49 -0400

    Updates

---
+++ precache-manifest.1cd1e673e5ce2eccd3cd198be50ce05a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc6fd8ea50d187e4018b0cbc80ac0aac",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "32ab8efb72d9348ddd0a",
+    "url": "/portfolio/static/css/2.22921dbe.chunk.css"
+  },
+  {
+    "revision": "565aa6245f45e9b57b1f",
+    "url": "/portfolio/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "32ab8efb72d9348ddd0a",
+    "url": "/portfolio/static/js/2.b6e1ad1c.chunk.js"
+  },
+  {
+    "revision": "565aa6245f45e9b57b1f",
+    "url": "/portfolio/static/js/main.c794e15a.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  }
+]);
